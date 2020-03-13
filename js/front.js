22d88c3926e289de7c3f907d062170a1abaa037f
BeaK94 <beatakusnierz94@gmail.com>
2020-03-13 13:36:26 +0200

    More playing with GTM

--- js/front.js
+++ js/front.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
   // my code - exercises
   // product page has pathname /detail.html
 
@@ -17,124 +17,244 @@ $(function() {
 
   /* if pathname is /index.html, trigger view:HomePage event etc.*/
 
+  /* MY VERSION OF CODE */
+  /*  function getPageName() {
+     var pathname = window.location.pathname;
+     if (pathname === "/index.html") {
+       return "HomePage";
+     } else if (pathname.indexOf("detail.html") > -1) {
+       //the pathname includes '', not necessarily is exactly ''
+       return "ProductPage";
+     } else if (pathname.indexOf("basket.html") > -1) {
+       return "Basket";
+     } else if (pathname.indexOf("checkout1.html") > -1) {
+       return "Checkout";
+     } else if (pathname.indexOf("checkout2.html") > -1) {
+       return "Delivery";
+     } else if (pathname.indexOf("checkout3.html") > -1) {
+       return "Payment";
+     } else if (pathname.indexOf("checkout4.html") > -1) {
+       return "OrderReview";
+     } else {
+       return "";
+     }
+   }
+ 
+   function getProductInfo() {
+     //get product name, put it nicely into an object
+     return {
+       productName: $("#productMain h1").text(),
+       productPrice: $("#productMain .price").text()
+     };
+   }
+ 
+ 
+   function getCartInfo() {
+     var _products = [];
+ 
+     // select the product info and push it into the array _products
+ 
+     document.querySelectorAll("#checkout tbody tr").forEach(function (product) {
+       // for each row, that is a product on the list get the name, price and quantity (get all the cells of a row, take the second for the name, third for quantity, fourth for price, choose their inner text)
+       var _name = product.querySelectorAll("td")[1].querySelector("a")
+         .innerHTML;
+       var _prodPrice = product.querySelectorAll("td")[3].innerHTML;
+       var _quantity = product.querySelectorAll("td")[2].innerHTML;
+       var _discount = product.querySelectorAll("td")[4].innerHTML;
+       var _price = product.querySelectorAll("td")[5].innerHTML;
+ 
+       // make an object of each product/row
+       var _product = {
+         productName: _name,
+         productPrice: _prodPrice,
+         productQuantity: _quantity,
+         discount: _discount,
+         price: _price
+       };
+       // add product object into the objects array
+       _products.push(_product);
+     });
+ 
+     return {
+       totalPrice: $("tfoot th")[1].innerHTML,
+       productList: _products,
+       userMachine: navigator.userAgent
+     };
+   }
+ 
+   function getParam() {
+     //get extra parameters to pass to triggerPageEvent function
+     var pageName = getPageName();
+     var result = null; // when neither of the ifs is relevant to the page, return null, so that triggerPageEvent works as if without extra param, cause no param = null in this case
+ 
+     if (pageName === "ProductPage") {
+       result = getProductInfo();
+       return result;
+     } else if (pageName === "OrderReview") {
+       result = getCartInfo();
+       return result;
+     }
+ 
+     return result; // for the pages with no extra params it is null
+   }
+ 
+   function triggerPageEvent() {
+     var pageName = getPageName();
+     var params = getParam();
+ 
+     if (pageName === "") {
+       return; // don't trigger anything if the pathname is empty
+     }
+ 
+     if (pageName === "OrderReview") {
+       $(".box-footer [type='submit']").click(function () {
+         $(document).trigger("conversion", params);
+       });
+       return;
+     }
+ 
+     // if the page location is not empty and is not OrderReview, trigger view:[pageName]
+     $(document).trigger("view:" + pageName, params);
+     //e.g. $(document).trigger('view:ProductPage', {productName: blah, productPrice: 100});
+   }
+ 
+   $(document).on("view:ProductPage", function (event, params) {
+     console.log("The first parameter received: ");
+     console.log(event);
+     console.log("The second parameter received: ");
+     console.log(params);
+     
+     ga('send', 'event', ‘ProductPage', ‘View’, params.productName, {
+     
+       nonInteraction: true
+     
+     });
+    
+   });
+ 
+   triggerPageEvent(); */
+
+  /* END OF MY OWN CODE */
+
+  /* HOANG'S VERSION OF CODE AND FURTHER EXERCISES */
+
   function getPageName() {
     var pathname = window.location.pathname;
-    if (pathname === "/index.html") {
-      return "HomePage";
-    } else if (pathname.indexOf("detail.html") > -1) {
-      //the pathname includes '', not necessarily is exactly ''
-      return "ProductPage";
-    } else if (pathname.indexOf("basket.html") > -1) {
-      return "Basket";
-    } else if (pathname.indexOf("checkout1.html") > -1) {
-      return "Checkout";
-    } else if (pathname.indexOf("checkout2.html") > -1) {
-      return "Delivery";
-    } else if (pathname.indexOf("checkout3.html") > -1) {
-      return "Payment";
-    } else if (pathname.indexOf("checkout4.html") > -1) {
-      return "OrderReview";
-    } else {
-      return "";
+    if (pathname.indexOf('index.html') > -1) {
+      return 'HomePage';
+    } else if (pathname.indexOf('detail.html') > -1) {
+      return 'ProductPage';
+    } else if (pathname.indexOf('checkout4.html') > -1) {
+      return 'Checkout4';
     }
   }
 
   function getProductInfo() {
-    //get product name, put it nicely into an object
     return {
-      productName: $("#productMain h1").text(),
-      productPrice: $("#productMain .price").text()
+      productName: $('#productMain h1.text-center').text(),
+      productPrice: $('#productMain .price').text()
     };
+  }
+
+  function generateUniqueId(length) {
+    // length is how many characters we want in the ID
+    var result = "";
+    var characters =
+      "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
   }
 
   function getCartInfo() {
-    var _products = [];
+    var productInfoEls = $('#checkout table tbody tr');
+    var result = {};
 
-    // select the product info and push it into the array _products
+    /* original version
+    result.totalPurchase = $('#checkout table tfoot th').eq(1).text();
+    result.userAgent = navigator.userAgent; // seeing the users machine details, browser etc
+    result.productList = []; */
 
-    document.querySelectorAll("#checkout tbody tr").forEach(function(product) {
-      // for each row, that is a product on the list get the name, price and quantity (get all the cells of a row, take the second for the name, third for quantity, fourth for price, choose their inner text)
-      var _name = product.querySelectorAll("td")[1].querySelector("a")
-        .innerHTML;
-      var _prodPrice = product.querySelectorAll("td")[3].innerHTML;
-      var _quantity = product.querySelectorAll("td")[2].innerHTML;
-      var _discount = product.querySelectorAll("td")[4].innerHTML;
-      var _price = product.querySelectorAll("td")[5].innerHTML;
+    // putting the info into format good for GTM and GA
+    result.actionField = {
+      id: generateUniqueId(16), // I want ID with 16 characters
+      revenue: $('#checkout table tfoot th').eq(1).text().match(/\d+/g).join('.')
+      // probably has to be called revenue and not totalPrice, get text, but we only want the number, not $, so get numbers out of it and join them with a '.'
+    }
 
-      // make an object of each product/row
-      var _product = {
-        productName: _name,
-        productPrice: _prodPrice,
-        productQuantity: _quantity,
-        discount: _discount,
-        price: _price
-      };
-      // add product object into the objects array
-      _products.push(_product);
+    result.products = [];
+
+    $.each(productInfoEls, function (index, el) {
+      result.products.push({
+        productName: $(el).children().eq(1).text(),
+        quantity: $(el).children().eq(2).text(),
+        productPrice: $(el).children().eq(3).text(),
+        discount: $(el).children().eq(4).text(),
+        totalPrice: $(el).children().eq(5).text()
+      });
     });
 
-    return {
-      totalPrice: $("tfoot th")[1].innerHTML,
-      productList: _products,
-      userMachine: navigator.userAgent
-    };
+    return result;
   }
 
   function getParam() {
-    //get extra parameters to pass to triggerPageEvent function
     var pageName = getPageName();
-    var result = null; // when neither of the ifs is relevant to the page, return null, so that triggerPageEvent works as if without extra param, cause no param = null in this case
+    var result = null;
 
-    if (pageName === "ProductPage") {
+    if (pageName === 'ProductPage') {
       result = getProductInfo();
       return result;
-    } else if (pageName === "OrderReview") {
+    } else if (pageName === 'Checkout4') {
       result = getCartInfo();
       return result;
     }
 
-    return result; // for the pages with no extra params it is null
+    return result;
   }
 
   function triggerPageEvent() {
     var pageName = getPageName();
     var params = getParam();
 
-    if (pageName === "") {
-      return; // don't trigger anything if the pathname is empty
-    }
+    if (pageName === 'Checkout4') {
+      // put data that we want to be tracked into format good for GTM and GA
+      window.dataLayer.push({
+        'ecommerce': {
+          'purchase': params
+          // params or getCartInfo(), cause they should return the same
+        }
+      })
 
-    if (pageName === "OrderReview") {
-      $(".box-footer [type='submit']").click(function() {
-        $(document).trigger("conversion", params);
+      // specific event listener for checkout4 Page
+      $('#checkout button').on('click', function () {
+        $(document).trigger('conversion', params);
       });
-      return;
+    } else {
+      // i fthe pagename if anything else than checkout4
+      $(document).trigger('view:' + pageName, params);
     }
-
-    // if the page location is not empty and is not OrderReview, trigger view:[pageName]
-    $(document).trigger("view:" + pageName, params);
-    //e.g. $(document).trigger('view:ProductPage', {productName: blah, productPrice: 100});
   }
 
-  $(document).on("view:ProductPage", function(event, params) {
-    console.log("The first parameter received: ");
+
+  $(document).on('view:ProductPage', function (event, params) {
+    console.log('The first parameter that I received is: ');
     console.log(event);
-    console.log("The second parameter received: ");
+
+    console.log('The second parameter that I received is: ')
     console.log(params);
-    /*
-    ga('send', 'event', ‘ProductPage', ‘View’, params.productName, {
-    
-      nonInteraction: true
-    
-    });
-    */
+
+    // ga('send', 'event', 'ProductPage', 'View', params.productName, {
+    //     nonInteraction: true
+    // });
   });
 
   triggerPageEvent();
 
-  //clicking on 'Place an order' triggers 'conversion'
-  $(".box-footer [type='submit']").click(function() {
-    /*  var extraParam = {
+  /* //clicking on 'Place an order' triggers 'conversion'
+  $(".box-footer [type='submit']").click(function () {
+     var extraParam = {
       {productName:
       productPrice:
     purchaseQuantity:
@@ -143,7 +263,7 @@ $(function() {
         productPrice:
       purchaseQuantity:
     totalPrice:}
-    }; */
+    };
     $(document).trigger("conversion", extraParam);
     console.log("user clicked the place an order button");
   });
@@ -154,7 +274,7 @@ $(function() {
   //
   //
   //
-  // end of my code
+  // end of my code */
 
   $(".shop-detail-carousel").owlCarousel({
     items: 1,
@@ -219,7 +339,7 @@ $(function() {
     alternateColour.attr("href", $.cookie("theme_csspath"));
   }
 
-  $("#colour").change(function() {
+  $("#colour").change(function () {
     if ($(this).val() !== "") {
       var theme_csspath = "css/style." + $(this).val() + ".css";
 
@@ -235,12 +355,12 @@ $(function() {
   });
 });
 
-$(window).on("load", function() {
+$(window).on("load", function () {
   $(this).alignElementsSameHeight();
 });
 
-$(window).resize(function() {
-  setTimeout(function() {
+$(window).resize(function () {
+  setTimeout(function () {
     $(this).alignElementsSameHeight();
   }, 150);
 });
@@ -287,27 +407,27 @@ function utils() {
   $("#checkout").on(
     "click",
     ".box.shipping-method, .box.payment-method",
-    function(e) {
+    function (e) {
       var radio = $(this).find(":radio");
       radio.prop("checked", true);
     }
   );
   /* click on the box activates the link in it */
 
-  $(".box.clickable").on("click", function(e) {
+  $(".box.clickable").on("click", function (e) {
     window.location = $(this)
       .find("a")
       .attr("href");
   });
   /* external links in new window*/
 
-  $(".external").on("click", function(e) {
+  $(".external").on("click", function (e) {
     e.preventDefault();
     window.open($(this).attr("href"));
   });
   /* animated scrolling */
 
-  $(".scroll-to, .scroll-to-top").click(function(event) {
+  $(".scroll-to, .scroll-to-top").click(function (event) {
     var full_url = this.href;
     var parts = full_url.split("#");
     if (parts.length > 1) {
@@ -334,8 +454,8 @@ function utils() {
   }
 }
 
-$.fn.alignElementsSameHeight = function() {
-  $(".same-height-row").each(function() {
+$.fn.alignElementsSameHeight = function () {
+  $(".same-height-row").each(function () {
     var maxHeight = 0;
 
     var children = $(this).find(".same-height");
@@ -343,7 +463,7 @@ $.fn.alignElementsSameHeight = function() {
     children.height("auto");
 
     if ($(document).width() > 768) {
-      children.each(function() {
+      children.each(function () {
         if ($(this).innerHeight() > maxHeight) {
           maxHeight = $(this).innerHeight();
         }
@@ -357,7 +477,7 @@ $.fn.alignElementsSameHeight = function() {
 
     children.height("auto");
 
-    children.each(function() {
+    children.each(function () {
       if ($(this).innerHeight() > maxHeight) {
         maxHeight = $(this).innerHeight();
       }
